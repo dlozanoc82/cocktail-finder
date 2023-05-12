7d2949a64f43b38cab8fac5af1485030a82f82da
Daniel <dlozanoc862@gmail.com>
2023-05-12 11:48:18 -0500

    :sparkles: Validation of the search for beverages form

--- src/components/FormSearch.jsx
+++ src/components/FormSearch.jsx
@@ -1,8 +1,40 @@
-import { Button, Form, Row, Col } from "react-bootstrap";
+import { Button, Form, Row, Col, Alert } from "react-bootstrap";
+import useCategories from "../hooks/useCategories";
+import { useState } from "react";
+
 
 const FormSearch = () => {
+
+    const [search, setSearch] = useState({
+        drink: '',
+        category: ''
+    })
+
+    const [alerta, setAlerta] = useState('');
+
+    const {drinksCategories} = useCategories();
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        if (Object.values(search).includes('')) {
+            setAlerta('All fields are required');
+            return;
+        }
+
+        setAlerta('');
+        
+
+    }
+
     return (
-        <Form>
+        <Form
+            onSubmit={handleSubmit}
+        >
+
+        {alerta && <Alert variant="danger" className="text-center">{alerta}</Alert>}
+
+
             <Row>
 
                 <Col md={6}>
@@ -14,6 +46,11 @@ const FormSearch = () => {
                             type="text"
                             placeholder="Ej: Tequila, Vodka, etc."
                             name="drink"
+                            value={search.drink}
+                            onChange={e => setSearch({
+                                ...search, 
+                                [e.target.name]: e.target.value
+                            })}
                         />
                     </Form.Group>
 
@@ -25,13 +62,39 @@ const FormSearch = () => {
                         <Form.Select
                             id="category"
                             name="category"
+                            value={search.category}
+                            onChange={e => setSearch({
+                                ...search, 
+                                [e.target.name]: e.target.value
+                            })}
                         >
                             <option>--- Choose category ---</option>
+                            {drinksCategories.map(category =>(
+                                <option
+                                    key={category.strCategory}
+                                    value={category.strCategory}
+                                >
+                                    {category.strCategory}
+                                </option>
+                            ))}
                         </Form.Select>
                     </Form.Group>
                 </Col>
 
             </Row>
+            
+            <Row className="justify-content-end">
+                <Col md={3}>
+                    <Button
+                        variant="danger"
+                        className="text-uppercase w-100"
+                        type="submit"
+                    >
+                        Search Beverage
+                    </Button>
+                </Col>
+            </Row>
+
         </Form>
     )
 }
